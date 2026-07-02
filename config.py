86cdd45b6d43443439335eaa41a3c5be9d383194
abigailljohnson3-jpg <abigailljohnson3@gmail.com>
2026-07-02 15:37:55 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+# Get your bot token from @BotFather on Telegram
+
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+
+# Note: For security, it's better to use environment variables on Railway
+# Instead of hardcoding your token here, you can:
+# import os
+# BOT_TOKEN = os.environ.get('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE')
